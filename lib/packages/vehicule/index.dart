7a070850c75437b5333af44edf4113ef3f2cce27
Dominique Youness <youness.dominique@gmail.com>
2022-07-07 13:28:39 +0200

    style : add card contains count

--- lib/packages/vehicule/index.dart
+++ lib/packages/vehicule/index.dart
@@ -14,8 +14,58 @@ class _vehiculeIndex extends State<vehiculeIndex>{
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            child: Column(
+              children: [
+                Text(
+                  'GESTION DES VEHICULES',
+                  style: TextStyle(
+                      fontSize: 20
+                  ),
+                ),
 
+                SizedBox(height: 8.0),
+
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      child: Card(
+                        elevation: 5.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Center(
+                            child: Text(
+                              '150 MARQUES'
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      child: Card(
+                        elevation: 5.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Center(
+                            child: Text(
+                                '120 ITEMS'
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
