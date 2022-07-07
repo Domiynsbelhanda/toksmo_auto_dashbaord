b9d3864cf2d0635522041aa1114ff2be04ae1c4e
Dominique Youness <youness.dominique@gmail.com>
2022-07-07 13:45:43 +0200

    style: bottom sheet add

--- lib/packages/vehicule/index.dart
+++ lib/packages/vehicule/index.dart
@@ -13,18 +13,54 @@ class _vehiculeIndex extends State<vehiculeIndex>{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(
+          'GESTION DES VEHICULES'
+        ),
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Container(
             child: Column(
               children: [
-                Text(
-                  'GESTION DES VEHICULES',
-                  style: TextStyle(
-                      fontSize: 20
-                  ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {
+                        showModalBottomSheet<void>(
+                          // context and builder are
+                          // required properties in this widget
+                          context: context,
+                          builder: (BuildContext context) {
+                            // we set up a container inside which
+                            // we create center column and display text
+
+                            // Returning SizedBox instead of a Container
+                            return SizedBox(
+                              height: 200,
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const <Widget>[
+                                      Text('GeeksforGeeks'),
+                                    ],
+                                ),
+                              ),
+                            );
+                          }
+                        );
+                      },
+                      child: Text('ADD MARQUE'),
+                    ),
+
+                    OutlinedButton(
+                      onPressed: () { },
+                      child: Text('ADD ITEM'),
+                    )
+                  ],
                 ),
 
                 SizedBox(height: 8.0),
