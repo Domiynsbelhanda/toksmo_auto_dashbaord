6e8f3117825d9b7e3917cae6c74aa56af1d950b5
Dominique Youness <youness.dominique@gmail.com>
2022-07-07 12:43:58 +0200

    feat : Create home page and add FirebaseAuth

---
+++ lib/packages/home/home.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomePage();
+  }
+}
+
+class _HomePage extends State<HomePage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // User is not signed in
+        if (!snapshot.hasData) {
+          // ...
+        }
+
+        // Render your application if authenticated
+        return Container();
+      },
+    );;
+  }
+}
